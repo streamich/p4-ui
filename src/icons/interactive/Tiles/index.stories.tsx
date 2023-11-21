3cd158031e92c7335ca0b22d8d731d7ef4a5e063
streamich <streamich@gmail.com>
2023-11-21 13:49:52 +0100

    feat: 🎸 add interactive icons

---
+++ src/icons/interactive/Tiles/index.stories.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import type {Meta, StoryObj} from '@storybook/react';
+import {Tiles as Component} from '.';
+
+const meta: Meta<typeof Text> = {
+  title: 'Icons/Interactive/Tiles',
+  component: Component,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {},
+};
+
+export default meta;
+
+export const Primary: StoryObj<typeof meta> = {
+  args: {
+  },
+};

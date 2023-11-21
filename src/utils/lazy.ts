1eeab4f7aec051432ea6de6bcf9bb470d8d348bd
streamich <streamich@gmail.com>
2023-11-21 15:34:37 +0100

    feat: 🎸 add <ScrollToView> and lazy() utilities

---
+++ src/utils/lazy.ts
@@ -0,0 +1,18 @@
+import {createElement as h, Suspense, lazy as reactLazy, ReactNode, FC} from 'react';
+
+const isClient = typeof window === 'object';
+
+export const lazy = <P extends {}>(
+  promiseGenerator: () => Promise<{default: React.ComponentType<P>}>,
+  fallbackDefault = null,
+): React.FC<P & {fallback?: any}> => {
+  if (!isClient) return (() => fallbackDefault) as any;
+
+  const Comp = reactLazy(promiseGenerator);
+  const Result: FC<P & {fallback?: ReactNode}> = (props) => {
+    const fallback = props.fallback || fallbackDefault;
+    return h(Suspense, {fallback}, h(Comp, props as any));
+  };
+
+  return Result;
+};

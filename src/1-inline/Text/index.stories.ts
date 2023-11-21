b9761d101edaee5faef1908a75f3c7eceb33dad7
streamich <streamich@gmail.com>
2023-11-21 12:47:27 +0100

    feat: 🎸 update <Text> story

--- src/1-inline/Text/index.stories.ts
+++ src/1-inline/Text/index.stories.ts
@@ -2,17 +2,19 @@ import type {Meta, StoryObj} from '@storybook/react';
 
 import {Text} from '.';
 
-const meta = {
-  title: 'Example/Text',
+const meta: Meta<typeof Text> = {
+  title: '1. Inline/Text',
   component: Text,
   parameters: {
     layout: 'centered',
   },
   tags: ['autodocs'],
-  // argTypes: {
-  //   backgroundColor: { control: 'color' },
-  // },
-} satisfies Meta<typeof Text>;
+  argTypes: {
+    children: { control: 'text' },
+    as: { control: 'select', options: ['p', 'span', 'div'] },
+    nowrap: { control: 'boolean' },
+  },
+};
 
 export default meta;
 type Story = StoryObj<typeof meta>;
